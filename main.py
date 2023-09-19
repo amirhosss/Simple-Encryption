6d803ba62f229e7b7edae797fd40c0a29268a77e
amirhosss <amirhosseinleo@gmail.com>
2023-09-19 11:18:03 +0330

    Some formatting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import base64
 
 import typer
-from cryptography.fernet import Fernet, InvalidToken
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
@@ -15,10 +15,7 @@ def encrypt(data: str, password: str, iterations: int = 480_000):
     salt = os.urandom(16)
 
     kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=iterations
+        algorithm=hashes.SHA256(), length=32, salt=salt, iterations=iterations
     )
     key = base64.urlsafe_b64encode(kdf.derive(password))
 
@@ -29,11 +26,10 @@ def encrypt(data: str, password: str, iterations: int = 480_000):
         file.writelines([salt, b"\n", encrypted_data])
 
 
-
 @app.command()
 def decrypt(password: str, iterations: int = 480_000):
-    password = bytes(password, 'utf-8')
-    with open('encrypted-data.txt', 'rb') as file:
+    password = bytes(password, "utf-8")
+    with open("encrypted-data.txt", "rb") as file:
         salt = file.readline().rstrip()
         encrypted_data = file.readline()
 
@@ -51,7 +47,9 @@ def decrypt(password: str, iterations: int = 480_000):
         decrypted_data = f.decrypt(encrypted_data)
         print("Successful. Your data is:", str(decrypted_data, "utf-8"), sep="\n")
     except Exception:
-        print(f"Wrong password or corrupted data. Please check your password or iterations")
+        print(
+            f"Wrong password or corrupted data. Please check your password or iterations"
+        )
 
 
 if __name__ == "__main__":
