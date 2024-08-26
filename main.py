61689cbc779ea5dfdd07052a3887cc07dc14e40b
Amirhossein <amirhosseinleo@gmail.com>
2024-08-26 19:28:59 +0330

    Some Enhancement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-app = typer.Typer()
+app = typer.Typer(rich_markup_mode=None, pretty_exceptions_enable=False)
 
 
 @app.command(help="Encrypt your data and make a file output")
@@ -29,26 +29,25 @@ def encrypt(data: str, password: str, iterations: int = 480_000):
 @app.command(help="Decrypt your data and print data in stdout")
 def decrypt(password: str, iterations: int = 480_000):
     password = bytes(password, "utf-8")
-    with open("encrypted-data.txt", "rb") as file:
-        salt = file.readline().rstrip()
-        encrypted_data = file.readline()
-
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=iterations,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password))
-
-    f = Fernet(key)
 
     try:
-        decrypted_data = f.decrypt(encrypted_data)
-        print("Successful. Your data is:", str(decrypted_data, "utf-8"), sep="\n")
-    except Exception:
+        file_name = "encrypted-data.txt"
+        with open(file_name, "rb") as file:
+            salt = file.readline().rstrip()
+            encrypted_data = file.readline()
+            kdf = PBKDF2HMAC(
+                algorithm=hashes.SHA256(), length=32, salt=salt, iterations=iterations
+            )
+            key = base64.urlsafe_b64encode(kdf.derive(password))
+
+            f = Fernet(key)
+            decrypted_data = f.decrypt(encrypted_data)
+            print("Successful. Your data is:", str(decrypted_data, "utf-8"), sep="\n")
+    except FileNotFoundError as e:
+        print(f"The file '{file_name}' not found. {e}")
+    except Exception as e:
         print(
-            f"Wrong password or corrupted data. Please check your password or iterations"
+            f"Wrong password or corrupted data. Please check your password or iterations. {e}"
         )
 
 
